af14996fb431b7970b469f7bed8d2a9d674bee3e
Thiago Lima <thenriq@gmail.com>
2021-09-02 21:37:27 +0100

    npm scripts - part 1

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('#mycarousel').carousel({ interval: 2000 });
+    $('#carouselButton').click(function(){
+        if ($('#carouselButton').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')){
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+});
+
+
+$(document).click(function(){
+    $("#BtnReserveTable").click(function(){
+        $("#reserveform").modal();
+    });
+
+});
+
+$(document).click(function(){
+    $("#BtnLogin").click(function(){
+        $("#loginModal").modal();
+    });
+});
+
+/*        Another clever way to call the modal:        
+     $('a[href$="#BtnLogin"]').on ( "click", function(){
+         $("#LoginModal").modal();
+     });        
+*/
